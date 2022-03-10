d83fec5acaf0acc6b6e2dcd0fbc3980a92eaf07c
aydenapollo <mcdonaldshane33@gmail.com>
2022-03-09 22:04:39 -0800

    project set up before redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cart from './Components/Cart/Cart';
+import Layout from './Components/Layout/Layout';
+import Products from './Components/Shop/Products';
+
+function App() {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
