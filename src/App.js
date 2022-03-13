348a444b3c55dd69a962af5865a2330b40e6b3e2
Shane-McDonald33 <mcdonaldshane33@gmail.com>
2022-03-13 11:57:05 -0500

    more logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Cart from './Components/Cart/Cart';
 import Layout from './Components/Layout/Layout';
 import Products from './Components/Shop/Products';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible)
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
--- src/Components/Cart/CartButton.jsx
+++ src/Components/Cart/CartButton.jsx
@@ -12,10 +12,17 @@
 // export default CartButton;
 
 import classes from './CartButton.module.css';
+import { UIActions } from '../../Store/ui-slice';
+import { useDispatch } from 'react-redux'
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCartHandler = (event) => {
+    event.preventDefault();
+    dispatch(UIActions.toggle())
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
