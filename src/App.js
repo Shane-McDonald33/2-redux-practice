6a54744e6a67ed3c1b9a330217928bc37875683a
Shane-McDonald33 <mcdonaldshane33@gmail.com>
2022-03-14 21:41:58 -0500

    sending data to backend

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import Cart from './Components/Cart/Cart';
 import Layout from './Components/Layout/Layout';
 import Products from './Components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible)
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-posting-default-rtdb.firebaseio.com/cart.json', { method: 'PUT', body: JSON.stringify(cart) })
+  }, [cart]);
+  
   return (
     <Layout>
       {showCart && <Cart />}
